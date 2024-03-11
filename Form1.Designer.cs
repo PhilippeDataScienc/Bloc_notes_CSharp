8811e8b69510ddd502de4f7330c1d4fa9b2aac77
PhilippeDataScienc <philippe.acquier@gmail.com>
2024-03-11 14:43:04 +0100

    Menu design completed successfully

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -266,7 +266,8 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Notepad C#";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
